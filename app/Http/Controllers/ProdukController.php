8af38b6367bb5478eda1935644ccb6f10f7f97cf
FathimahAzzahraWinarno <fathimahazzahra425@gmail.com>
2025-05-07 15:07:57 +0700

    produk

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 
 class ProdukController extends Controller
 {
-    public function keloaProduk()
+    public function kelolaProduk()
     {
         $produks = Produk::all();
         return view('admin.kelolaProduk', [
             'produks' => $produks,
             'title' => 'Daftar Produk',
-            'active' => 'produk'
+            'active' => 'Produk'
         ]);
     }
 
